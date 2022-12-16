20374ddcd89a74c7f693725903df245684c02228
Ryota Ohhashi <ryota.ohhashi@accenture.com>
2022-12-16 15:53:15 +0900

    modified

--- src/js/main.ts
+++ src/js/main.ts
@@ -9,15 +9,28 @@ const sketch = (p: p5) => {
     y:0
   }
 
+  let imgPath: string = "/window2.png";
   let canvas;
+  let img: any;
+  let isClicked: boolean = false;
+
+  p.preload = () => {
+    img = p.loadImage(imgPath);
+  }
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     canvas = <HTMLCanvasElement>document.querySelector(".p5Canvas");
     bindMousemove(canvas);
+
+    p.image(img, 0, 0, p.windowWidth, p.windowHeight)
   };
 
   p.draw = () => {
+    if(isClicked === false) return;
+
+    //@ts-ignore
+    p.blendMode(p.REMOVE);
     p.fill(0);
     p.ellipse(mouse.x, mouse.y, 100, 100);
   };
@@ -27,9 +40,20 @@ const sketch = (p: p5) => {
     mouse.y = e.clientY;
   }
 
+  const mouseDown = () => {
+    isClicked = true;
+  }
+
+  const mouseUp = () => {
+    isClicked = false;
+  }
+
   const bindMousemove = (target: HTMLCanvasElement) => {
     target.addEventListener('mousemove', mouseMove);
   }
+
+  window.addEventListener("mousedown", mouseDown);
+  window.addEventListener("mouseup", mouseUp);
 };
 
 new p5(sketch);
